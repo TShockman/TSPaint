b3007cb3300eac2414a58c02ac588507af22a743
Thomas Shockman <tom.shockman@gmail.com>
2019-02-10 21:16:25 -0600

    Add search

--- src/app/modules/sidebar/Sidebar.jsx
+++ src/app/modules/sidebar/Sidebar.jsx
@@ -15,6 +15,11 @@ export default class Sidebar extends Component {
     importPainting: PropTypes.func.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {search: ''};
+  }
+
   handleNew = event => {
     const {newPainting} = this.props;
     newPainting();
@@ -28,20 +33,40 @@ export default class Sidebar extends Component {
   handleImport = event => {
     const {importPainting} = this.props;
     if (event.target.files.length > 0) {
-      console.log(event.target.files[0])
       importPainting({fileBlob: event.target.files[0]});
     }
     event.preventDefault();
   };
 
+  handleSearch = event => {
+    const {paintings} = this.props;
+    event.stopPropagation();
+    const search = event.target.value;
+
+    this.setState({search});
+  };
+
   render() {
     const {paintings} = this.props;
+    const {search} = this.state;
+    const lowerSearch = search.toLowerCase();
+    const filteredPaintings = paintings.filter(painting => {
+      if (painting.title.toLowerCase().includes(lowerSearch)) {
+        return true;
+      }
+      const filteredTags = painting.tags.filter(tag => {
+        if (tag.toLowerCase().includes(lowerSearch)) {
+          return true;
+        }
+      });
+      return filteredTags.length > 0;
+    });
     return (
       <div className="sidebar">
         <h1>My Paintings</h1>
+        <input type="text" id="search" name="search" onChange={this.handleSearch} value={search}/>
         <ul>
-          <li><button onClick={this.handleNew}>New Painting</button></li>
-          {paintings.map(painting => {
+          {filteredPaintings.map(painting => {
             const json = JSON.stringify(painting);
             const blob = new Blob([json], {type: "application/json"});
             const url  = URL.createObjectURL(blob);
@@ -54,6 +79,7 @@ export default class Sidebar extends Component {
             );
           })}
         </ul>
+        <button onClick={this.handleNew}>New Painting</button>
         <button>
           <input onChange={this.handleImport} type="file"/>
         </button>
